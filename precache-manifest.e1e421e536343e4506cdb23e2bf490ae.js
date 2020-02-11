9fb52521a664620d01f5465536d4710e76486203
Punkrockechidna <dan.kaplan@ieleadership.org>
2020-02-11 00:15:41 -0500

    Updates

---
+++ precache-manifest.e1e421e536343e4506cdb23e2bf490ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b381e6abb33d0ae3cd90a57fa0d2c7c",
+    "url": "/face_recognition/index.html"
+  },
+  {
+    "revision": "21d5b10a69bccee8947c",
+    "url": "/face_recognition/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "171b38719a598b69c4a3",
+    "url": "/face_recognition/static/css/main.88286e28.chunk.css"
+  },
+  {
+    "revision": "21d5b10a69bccee8947c",
+    "url": "/face_recognition/static/js/2.de0a61d5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/face_recognition/static/js/2.de0a61d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "171b38719a598b69c4a3",
+    "url": "/face_recognition/static/js/main.2e300725.chunk.js"
+  },
+  {
+    "revision": "d364ddb86fba9f4b0533",
+    "url": "/face_recognition/static/js/runtime-main.53dec2d8.js"
+  }
+]);
